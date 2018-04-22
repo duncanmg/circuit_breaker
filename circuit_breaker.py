6baec388691e7d7e74f44a372d4f3eccd6021c9b
Duncan Garland <duncan.garland@ntlworld.com>
2018-04-22 14:27:47 +0100

    First attempt at hooks to a database.

--- circuit_breaker.py
+++ circuit_breaker.py
@@ -7,6 +7,9 @@ class CircuitBreaker:
 
   failures = list()
 
+  sink = ""
+  source = ""
+
   def protect(self, external_call):
     if self.is_closed():
       try:
@@ -18,17 +21,38 @@ class CircuitBreaker:
       raise Exception('Circuit Breaker open')
     
   def is_closed(self):
-    while len(self.failures) and self.failures[0][0] < int(time.time()) - self.max_age:
-      self.failures.pop(0)
-    if len(self.failures) < self.threshold:
+    failures = self.get_failures()
+    count = 0
+
+    while len(failures) and failures[0][0] < int(time.time()) - self.max_age:
+      failures.pop(0)
+      count = count + 1
+
+    if count > 0:
+      self.set_failures(failures)
+
+    if len(failures) < self.threshold:
       return True
     else:
       return False
       
   def process_error(self, e):
-    self.failures.append((time.time(), e))
-    pass
+    failures = self.get_failures()
+    failures.append((time.time(), e))
+    self.set_failures(failures)
   
+  def get_failures(self):
+    if self.source:
+      return self.source()
+    else:
+      return self.failures
+
+  def set_failures(self, failures):
+    if self.sink:
+      self.sink(failures)
+    else:
+      self.failures = failures
+
   def reset(self):
     self.failures = list()
 
